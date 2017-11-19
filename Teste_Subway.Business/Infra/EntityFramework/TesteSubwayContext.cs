34377def3d2bdb8b12fa67970e316bebcecc3e78
adimar <adimarjr@gmail.com>
2017-11-19 18:14:04 -0200

    inclusao de factory para criação do contexto

--- Teste_Subway.Business/Infra/EntityFramework/TesteSubwayContext.cs
+++ Teste_Subway.Business/Infra/EntityFramework/TesteSubwayContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,17 +9,29 @@ namespace TesteSubway.Business.Infra.EntityFramework
 {
     public class TesteSubwayContext : DbContext
     {
+        public const string connectionString = "Server=(localdb)\\mssqllocaldb;Database=Database1;Trusted_Connection=True;MultipleActiveResultSets=true";
         public TesteSubwayContext(DbContextOptions<TesteSubwayContext> options) : base(options) { }
 
         public static TesteSubwayContext Create()
         {
             var optionsBuilder = new DbContextOptionsBuilder<TesteSubwayContext>();
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Database1;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer(connectionString);
 
             return new TesteSubwayContext(optionsBuilder.Options);
         }
 
         public virtual DbSet<Client> Clients { get; set; }
         public virtual DbSet<Purchase> Purchases { get; set; }
+    }
+
+    public class ContextFactory : IDesignTimeDbContextFactory<TesteSubwayContext>
+    {
+        public TesteSubwayContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TesteSubwayContext>();
+            optionsBuilder.UseSqlServer(TesteSubwayContext.connectionString);
+
+            return new TesteSubwayContext(optionsBuilder.Options);
+        }
     }
 }
